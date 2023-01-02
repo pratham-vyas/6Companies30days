4579e5a1d162b1835b11df4ebd4b9959741c6037
Pratham <pratham1651@gmail.com>
2023-01-02 13:57:38 +0530

    changes in microsoft 150

--- microsoft/150valuate Reverse Polish Notation.cpp
+++ microsoft/150valuate Reverse Polish Notation.cpp
@@ -1,20 +1,21 @@
+typedef long long ll;
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        unordered_map<string, function<int (int, int) > > map = {
-            { "+" , [] (int a, int b) { return a + b; } },
-            { "-" , [] (int a, int b) { return a - b; } },
-            { "*" , [] (int a, int b) { return a * b; } },
-            { "/" , [] (int a, int b) { return a / b; } }
+    ll evalRPN(vector<string>& tokens) {
+        unordered_map<string, function<ll (ll, ll) > > map = {
+            { "+" , [] (ll a, ll b) { return a + b; } },
+            { "-" , [] (ll a, ll b) { return a - b; } },
+            { "*" , [] (ll a, ll b) { return a * b; } },
+            { "/" , [] (ll a, ll b) { return a / b; } }
         };
-        std::stack<int> stack;
+        std::stack<ll> stack;
         for (string& s : tokens) {
             if (!map.count(s)) {
                 stack.push(stoi(s));
             } else {
-                int op1 = stack.top();
+                ll op1 = stack.top();
                 stack.pop();
-                int op2 = stack.top();
+                ll op2 = stack.top();
                 stack.pop();
                 stack.push(map[s](op2, op1));
             }
