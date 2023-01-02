6d55411b7a8c61ea23a9c7b9fc1bfb5cbd66fd94
Pratham <pratham1651@gmail.com>
2023-01-02 14:59:57 +0530

    stack to st in microsoft 150

--- microsoft/150valuate Reverse Polish Notation.cpp
+++ microsoft/150valuate Reverse Polish Notation.cpp
@@ -7,18 +7,18 @@ public:
             { "*" , [] (int a, int b) { return a * b; } },
             { "/" , [] (int a, int b) { return a / b; } }
         };
-        std::stack<int> stack;
+        std::stack<int> st;
         for (string& s : tokens) {
             if (!map.count(s)) {
-                stack.push(stoi(s));
+                st.push(stoi(s));
             } else {
                 int op1 = stack.top();
-                stack.pop();
-                int op2 = stack.top();
-                stack.pop();
-                stack.push(map[s](op2, op1));
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                st.push(map[s](op2, op1));
             }
         }
-        return stack.top();
+        return st.top();
     }
 };
